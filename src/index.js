f1994fb2adaf829c214ec113762351474db1fe4a
stigmatory15 <repka_dmitriy@mail.ru>
2018-09-25 18:04:11 +0300

    task 4 complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    let a1 = equation.replace("^2", "");
+    let a2 = a1.replace(/x/g, "");
+    let a3 = a2.replace(/ /g, '');
+    let b = a3.split('*');
+    let d = b[1] * b[1] - 4 * b[0] * b[2];
+    let x1 = (-(b[1]) - Math.sqrt(d)) / (2 * b[0]);
+    let x2 = (-(b[1]) + Math.sqrt(d)) / (2 * b[0]);
+    let answer = [Math.round(x1), Math.round(x2)];
+    answer.sort( function(a, b) {return a - b} );
+    return answer;
 }
